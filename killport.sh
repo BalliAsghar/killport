db8ca31f6890c195bf2ef9e36c1efc8122e07f56
Balli Asghar <ballihacker@outlook.com>
2023-02-16 05:48:39 +0000

    improved

--- killport.sh
+++ killport.sh
@@ -1,43 +1,42 @@
 #!/bin/bash
 
-# Kill a process listening on a given port
+set -euo pipefail
 
-# Usage: killport <port>
-# Example: killport 3000
-# Author: https://github.com/BalliAsghar
+readonly os=$(uname)
 
-# Get the operating system
-os=$(uname)
+kill_port() {
+    local port=$1
 
-# Get the port number
-port=$1
+    # Validate the port number
+    if ! [[ "$port" =~ ^[0-9]{1,5}$ && "$port" -ge 0 && "$port" -le 65535 ]]; then
+        echo "Invalid port number"
+        exit 1
+    fi
+
+    # Check if port is in use
+    if ! type lsof >/dev/null 2>&1 || ! lsof -Pi :"$port" -sTCP:LISTEN -t >/dev/null; then
+        echo "Port $port is not in use"
+        exit 1
+    fi
+
+    # Kill the process
+    if [ "$os" == "Darwin" ]; then
+        # macOS
+        lsof -ti tcp:"$port" | xargs kill
+    else
+        # Linux
+        fuser -k "$port"/tcp
+    fi
+
+    echo "Process on port $port killed"
+}
 
 # Check if port is provided
-if [ -z "$port" ]; then
+if [ $# -ne 1 ]; then
     echo "usage: killport <port>"
     exit 1
 fi
 
-# Validate the port number
-if ! [[ "$port" =~ ^[0-9]{1,5}$ && "$port" -ge 0 && "$port" -le 65535 ]]; then
-    echo "Invalid port number"
-    exit 1
-fi
+kill_port "$1"
 
-# Check if port is in use
-if ! lsof -Pi :"$port" -sTCP:LISTEN -t >/dev/null; then
-    echo "Port $port is not in use"
-    exit 1
-fi
-
-# Kill the process
-if [ "$os" == "Darwin" ]; then
-    # macOS
-    lsof -ti tcp:"$port" | xargs kill
-else
-    # Linux
-    fuser -k "$port"/tcp
-fi
-
-echo "Process on port $port killed"
 exit 0
